53818845a57a5e68f517911cf98eb478031b3aa6
Sebastian Moreno <sebastiancamilo40@gmail.com>
2023-09-19 20:40:37 -0500

    choose two numbers (terminado)

--- Choose_two_numbers/src/Main.java
+++ Choose_two_numbers/src/Main.java
@@ -3,36 +3,37 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner sc = new Scanner(System.in);
-		
-		boolean ar1=false,ar2=false, cont=false;
-		int num1=0,num2=0;
+		boolean cont=false;
 		int la1=sc.nextInt();
 		int[] array1 = new int[la1];
 		for(int i=0;i<la1;i++) array1[i]=sc.nextInt();
 		int la2=sc.nextInt();
 		int[] array2 = new int[la2];
-		for(int i=0; i<la2; i++) array2[i]=sc.nextInt();
-		
+		for(int i=0; i<la2; i++) array2[i]=sc.nextInt();	
 		for(int i=0; i<la1;i++) {
 			for(int j=0; j<la2; j++) {
-				int temp = array1[i]+array2[j];
-				//compara la suma con cada elemento de los arrays
+				boolean ar1=true,ar2=true;
+				int temp = array1[i]+array2[j];			
 				for(int k=0;k<la1;k++) {
-					if(temp==array1[k]) { 
-						ar1=true;
+					if(temp==array1[k]) {
+						ar1=false;
 						break;
-					}else num1=array1[k];
-					for(int g=0;g<la2;g++) {
-						if(temp==array2[g]) {
-							break;
-						}else num2=array2[g];	
 					}
 				}
+				for(int k=0;k<la2;k++) {
+					if(temp==array2[k]) {
+						ar2=false;
+						break;
+					}
+				}
+				if(ar1 && ar2) {
+					System.out.println(array1[i]+" "+array2[j]);
+					cont=true;
+					break;
+				}
 			}
+			if(cont) break;
 		}
-		System.out.println(num1+" "+num2);
 	}
 }
