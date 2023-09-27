80b909967238c3f0df05c282160229bb6a3a60f5
sebastian3095 <scamilomoreno@ucundinamarca.edu.co>
2023-09-27 08:22:17 -0500

    B-or-G version 1, odd aun sin terminar

---
+++ Boy_or_girl/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name= sc.next();		
+		char[] mm = name.toCharArray();
+		int con=0;
+		
+		for(int i=0;i<mm.length;i++) {
+			for(int j=0;j<mm.length;j++) {
+				if(i!=j && mm[i]==mm[j]) con++;
+			}
+		}
+		System.out.println(((mm.length-(con/2))%2==0 )?"CHAT WITH HER!":"IGNORE HIM!");
+	}
+
+}
